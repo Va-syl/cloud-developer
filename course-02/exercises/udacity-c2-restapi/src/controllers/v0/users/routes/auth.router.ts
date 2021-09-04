2d3ff59859f9526cb834f2f54f44e6b04aced713
Va-syl <nathan.winiarczyk@gmail.com>
2021-09-04 11:29:05 -0500

    lesson 5.4 bcrypt

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -11,13 +11,13 @@ import * as EmailValidator from 'email-validator';
 const router: Router = Router();
 
 async function generatePassword(plainTextPassword: string): Promise<string> {
-    //@TODO Use Bcrypt to Generated Salted Hashed Passwords
-    return "NotYetImplemented"
+    let rounds = 10;
+    const salt = await bcrypt.genSalt(rounds);
+    return await bcrypt.hash(plainTextPassword, salt);
 }
 
 async function comparePasswords(plainTextPassword: string, hash: string): Promise<boolean> {
-    //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-    return true
+    return await (bcrypt.compare(plainTextPassword, hash));
 }
 
 function generateJWT(user: User): string {
